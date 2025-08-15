d3cd47cc6f47a14d1bb1319ba81f7440e19cc0d2
Nicolas <nicolasrodriguesveloso@gmail.com>
2025-08-15 16:50:08 -0300

    feat: implemented JwtUtils and AuthEntryPoint classes

---
+++ src/main/java/br/com/nicolas/projeto_user_authentication/security/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package br.com.nicolas.projeto_user_authentication.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+
+@Configuration
+@EnableMethodSecurity
+public class WebSecurityConfig {
+}
---
+++ src/main/java/br/com/nicolas/projeto_user_authentication/security/jwt/JwtUtils.java
@@ -0,0 +1,50 @@
+package br.com.nicolas.projeto_user_authentication.security.jwt;
+
+import br.com.nicolas.projeto_user_authentication.service.UserDetailsImpl;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtils {
+
+    @Value("${projeto.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${projeto.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    public String generateTokenFromUserDetailsImpl(UserDetailsImpl userDetails) {
+        return Jwts.builder().setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + jwtExpirationMs))
+                .signWith(getSigninKey(), SignatureAlgorithm.HS512).compact();
+    }
+
+    public Key getSigninKey() {
+        SecretKey key = Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+        return key;
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parser().setSigningKey(getSigninKey()).build().parseClaimsJws(authToken);
+            return true;
+        } catch (MalformedJwtException e) {
+            System.out.println("Invalid token " + e.getMessage());
+        } catch (ExpiredJwtException e) {
+            System.out.println("Expired token " + e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            System.out.println("Unsupported token " + e.getMessage());
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid argument token " + e.getMessage());
+        }
+        return false;
+    }
+}
